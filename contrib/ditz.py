2fe03d8e96f9a351803269507a7d93c00162ac3e
Glenn Hutchings <zondo42@googlemail.com>
2010-04-07 17:19:00 +0100

    Read the Ditz config file to locate things.

--- contrib/ditz.py
+++ contrib/ditz.py
@@ -7,27 +7,61 @@ import yaml
 import glob
 
 class Ditz(object):
-    def __init__(self, issuedir = "issues"):
-        self.issuedir = issuedir
+    def __init__(self, basedir = None):
+        # Set base directory for searching.
+        self.basedir = basedir if basedir else os.getcwd()
 
-        path = os.path.join(issuedir, Project.filename)
-        if os.path.exists(path):
-            self.project = self._read(path)
-        else:
-            raise DitzError("'%s' is not a Ditz issue directory" % issuedir)
+        # Find and read the Ditz config file.
+        path = self._find(Config.filename)
+        self.config = self._read(path)
+
+        # Extract the issue directory name.
+        try:
+            dirname = self.config.issue_dir
+        except AttributeError:
+            raise DitzError("'%s' does not define 'issue_dir'" % path)
+
+        # Find the issue directory.
+        self.issuedir = self._find(dirname)
+
+        # Read the project file.
+        path = os.path.join(self.issuedir, Project.filename)
+        self.project = self._read(path)
 
     def __iter__(self):
+        """Iterate over all issues."""
+
         match = os.path.join(self.issuedir, Issue.template % "*")
         for path in glob.glob(match):
             yield self._read(path)
 
+    def _find(self, filename):
+        """Find a filename in or above the base directory."""
+
+        curdir = self.basedir
+
+        while True:
+            path = os.path.join(curdir, filename)
+            if os.path.exists(path):
+                return path
+            elif curdir != "/":
+                curdir = os.path.split(curdir)[0]
+            else:
+                raise DitzError("can't find '%s' in or above '%s'"
+                                % (filename, self.basedir))
+
     def _read(self, path):
+        """Read YAML data from a file."""
         return yaml.safe_load(open(path))
 
     def _write(self, data, path):
+        """Write YAML data to a file."""
+
         fp = open(path, "w")
         data = yaml.dump(data, fp, default_flow_style = False)
         fp.close()
+
+class DitzError(Exception): pass
 
 class DitzObject(yaml.YAMLObject):
     yaml_loader = yaml.SafeLoader
@@ -55,19 +89,21 @@ class Issue(DitzObject):
     def filename(self):
         return self.template % self.id
 
-class DitzError(Exception): pass
-
 if __name__ == "__main__":
     ditz = Ditz()
 
+    print "%s <%s>" % (ditz.config.name, ditz.config.email)
+
+    print
     print ditz.project.name
 
+    print
     print "Releases:"
     for release in ditz.project.releases:
         date = release.release_time 
-        print "   %s [%s]" % (release.name,
-                              date if date else "unreleased")
+        print "   %s [%s]" % (release.name, date if date else "unreleased")
 
+    print
     print "Issues:"
     for issue in ditz:
         print "   %s [%s]" % (issue.title, issue.type)
